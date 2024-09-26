3b834bd1f8057fbb92b9d8df96f3b436cb4f1f4f
Muhammad Shoaib Mushtaq <shoaibfattani2010@gmail.com>
2024-09-27 00:28:59 +0500

    setting up environment for react native and creation of react native cli boilder plate

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+// const styles = StyleSheet.create({
+//   bigfont: {
+//     fontSize: '40px',
+//   },
+// });
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello, React Native!</Text>
+    </View>
+  );
+};
+
+export default App;
